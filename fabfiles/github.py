55370eb6c7c324a0ab804eab1204a8555713ded9
Ivan Savov <ivan.savov@gmail.com>
2020-08-06 19:50:54 -0400

    Improved github chef repos (before-refactor)

--- fabfiles/github.py
+++ fabfiles/github.py
@@ -62,10 +62,11 @@ def create_github_repo(nickname, source_url=None, init=True, private=False):
 
 
 @task
-def list_chef_repos(organization='learningequality'):
+def list_chef_repos(organization='learningequality', printing=True, detailed=False):
     """
     List of all learningequality github repos that match the `sushi-chef-*`.
     """
+    detailed = (detailed and detailed.lower() == 'true')
     CHEF_REPO_PATTERN = re.compile('.*sushi-chef-.*')
     github = get_github_client() 
     le_org = github.get_organization(organization)
@@ -75,12 +76,42 @@ def list_chef_repos(organization='learningequality'):
         if CHEF_REPO_PATTERN.search(repo.name):
             chef_repos.append(repo)
     for repo in chef_repos:
+        forks = list(repo.get_forks())
+        branches = list(repo.get_branches())
         pulls = list(repo.get_pulls())
-        issues = list(repo.get_issues())
-        print(repo.name,
-              '\t', repo.html_url,
-              '\t', len(pulls), 'PRs',
-              '\t', len(issues), 'Issues')
+        issues = list(repo.get_issues(state='open'))
+        if printing:
+            if detailed:
+                print()  # extra newline between repos
+            print('-', blue(repo.html_url),
+                '\t', len(forks), 'forks',
+                '\t', len(branches), 'branches',
+                '\t', len(pulls), 'PRs',
+                '\t', len(issues), 'Issues')
+            if detailed:
+                for fork in forks:
+                    fork_branches = list(fork.get_branches())
+                    branch_names = [yellow(fb.name) for fb in fork_branches if fb.name != 'master']
+                    fork_branches_str = 'branches: ' + ', '.join(branch_names) if branch_names else ''
+                    print(blue('   - fork: ' + fork.owner.login + '/' + fork.name), fork_branches_str)
+                for branch in branches:
+                    commit_msg_lines = branch.commit.commit.message.split('\n')
+                    commit_msg_str = commit_msg_lines[0] 
+                    print(yellow('   - branch: ' + branch.name),
+                           '('+ branch.commit.sha[0:7]+')', 
+                            'by', branch.commit.author.login if branch.commit.author else '?',
+                            commit_msg_str, '\t', branch.commit.commit.last_modified)
+                for pr in pulls:
+                    print(green('   - PR' + str(pr.number) + ': ' + pr.title),
+                          pr.state, 
+                          'by', pr.user.login,
+                          '\t', pr.last_modified,
+                          pr.commits, 'commits',
+                          pr.comments, 'comments',
+                          pr.labels if pr.labels else '')
+                for issue in issues:
+                    print(red('   - I' + str(issue.number) + ': ' + issue.title),
+                          issue.state, issue.comments, 'comments', issue.labels if issue.labels else '')
     return chef_repos
 
 
